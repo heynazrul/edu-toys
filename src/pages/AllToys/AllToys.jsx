bcac333271758f9645e74edd2c1ae03d099cc145
Nazrul <heynazrul@gmail.com>
2023-05-20 20:53:41 +0600

    added modal on all toys  page

--- src/pages/AllToys/AllToys.jsx
+++ src/pages/AllToys/AllToys.jsx
@@ -1,77 +1,35 @@
-import { PencilIcon } from '@heroicons/react/24/solid';
-import { ArrowDownTrayIcon, MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import {
   Card,
   CardHeader,
   Typography,
   Button,
   CardBody,
-  Chip,
   CardFooter,
   Avatar,
   IconButton,
-  Tooltip,
   Input,
 } from '@material-tailwind/react';
+import { Link, useLoaderData } from 'react-router-dom';
 
-const TABLE_HEAD = ['Toy Name', 'Amount', 'Date', 'Status', 'Account', ''];
-
-const TABLE_ROWS = [
-  {
-    img: '/img/logos/logo-spotify.svg',
-    name: 'Spotify',
-    amount: '$2,500',
-    date: 'Wed 3:00pm',
-    status: 'paid',
-    account: 'visa',
-    accountNumber: '1234',
-    expiry: '06/2026',
-  },
-  {
-    img: '/img/logos/logo-amazon.svg',
-    name: 'Amazon',
-    amount: '$5,000',
-    date: 'Wed 1:00pm',
-    status: 'paid',
-    account: 'master-card',
-    accountNumber: '1234',
-    expiry: '06/2026',
-  },
-  {
-    img: '/img/logos/logo-pinterest.svg',
-    name: 'Pinterest',
-    amount: '$3,400',
-    date: 'Mon 7:40pm',
-    status: 'pending',
-    account: 'master-card',
-    accountNumber: '1234',
-    expiry: '06/2026',
-  },
-  {
-    img: '/img/logos/logo-google.svg',
-    name: 'Google',
-    amount: '$1,000',
-    date: 'Wed 5:00pm',
-    status: 'paid',
-    account: 'visa',
-    accountNumber: '1234',
-    expiry: '06/2026',
-  },
-  {
-    img: '/img/logos/logo-netflix.svg',
-    name: 'netflix',
-    amount: '$14,000',
-    date: 'Wed 3:30am',
-    status: 'cancelled',
-    account: 'visa',
-    accountNumber: '1234',
-    expiry: '06/2026',
-  },
-];
+import { FaPlus } from 'react-icons/fa';
+import { useState } from 'react';
+import DetailsModal from '../shared/DetailsModal/DetailsModal';
 
 const AllToys = () => {
+  const [clickedID, setClickedID] = useState(null);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen((cur) => !cur);
+    // setClickedID(id);
+  };
+  console.log(clickedID);
+
+  const TABLE_HEAD = ['Toy Name', 'Sub-category', 'Seller Name', 'Price', 'Quantity', ''];
+  const TABLE_ROWS = useLoaderData();
+
   return (
-    <Card className="h-full w-full">
+    <Card className="mx-auto my-12 h-full max-w-7xl">
       <CardHeader
         floated={false}
         shadow={false}
@@ -81,12 +39,12 @@ const AllToys = () => {
             <Typography
               variant="h5"
               color="blue-gray">
-              Recent Transactions
+              All Toys
             </Typography>
             <Typography
               color="gray"
               className="mt-1 font-normal">
-              These are details about the last transactions
+              All your product showcase in one place
             </Typography>
           </div>
           <div className="flex w-full shrink-0 gap-2 md:w-max">
@@ -96,20 +54,24 @@ const AllToys = () => {
                 icon={<MagnifyingGlassIcon className="h-5 w-5" />}
               />
             </div>
-            <Button
-              className="flex items-center gap-3"
-              color="blue"
-              size="sm">
-              <ArrowDownTrayIcon
-                strokeWidth={2}
-                className="h-4 w-4"
-              />{' '}
-              Download
-            </Button>
+            <Link
+              to={'/user/add-toy'}
+              className="inline-flex">
+              <Button
+                className="flex items-center gap-3"
+                color="blue"
+                size="sm">
+                <FaPlus
+                  strokeWidth={2}
+                  className="h-4 w-4"
+                />{' '}
+                Add New Toy
+              </Button>
+            </Link>
           </div>
         </div>
       </CardHeader>
-      <CardBody className="overflow-scroll px-0">
+      <CardBody className="overflow-x-scroll px-0">
         <table className="w-full min-w-max table-auto text-left">
           <thead>
             <tr>
@@ -128,25 +90,26 @@ const AllToys = () => {
             </tr>
           </thead>
           <tbody>
-            {TABLE_ROWS.map(({ img, name, amount, date, status, account, accountNumber, expiry }, index) => {
+            {TABLE_ROWS.map(({ sellerName, email, toyName, category, price, quantity, _id, photo }, index) => {
               const isLast = index === TABLE_ROWS.length - 1;
               const classes = isLast ? 'p-4' : 'p-4 border-b border-blue-gray-50';
 
               return (
-                <tr key={name}>
+                <tr key={toyName}>
                   <td className={classes}>
                     <div className="flex items-center gap-3">
                       <Avatar
-                        src={img}
-                        alt={name}
-                        size="md"
-                        className="border border-blue-gray-50 bg-blue-gray-50/50 object-contain p-1"
+                        src={photo}
+                        alt={toyName}
+                        size="lg"
+                        variant="square"
+                        className="border border-blue-gray-50 bg-blue-gray-50/50 object-cover p-1"
                       />
                       <Typography
                         variant="small"
                         color="blue-gray"
                         className="font-bold">
-                        {name}
+                        {toyName}
                       </Typography>
                     </div>
                   </td>
@@ -155,7 +118,39 @@ const AllToys = () => {
                       variant="small"
                       color="blue-gray"
                       className="font-normal">
-                      {amount}
+                      {category}
+                    </Typography>
+                  </td>
+                  {/* Seller Name */}
+                  <td className={classes}>
+                    <div className="flex flex-col">
+                      <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="font-normal">
+                        {sellerName}
+                      </Typography>
+                      <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="font-normal opacity-70">
+                        {email}
+                      </Typography>
+                    </div>
+                    {/* <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal">
+                      {date}
+                    </Typography> */}
+                  </td>
+                  {/* Price */}
+                  <td className={classes}>
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal">
+                      ${price}
                     </Typography>
                   </td>
                   <td className={classes}>
@@ -163,64 +158,36 @@ const AllToys = () => {
                       variant="small"
                       color="blue-gray"
                       className="font-normal">
-                      {date}
+                      {quantity}
                     </Typography>
                   </td>
                   <td className={classes}>
-                    <div className="w-max">
-                      <Chip
-                        size="sm"
-                        variant="ghost"
-                        value={status}
-                        color={status === 'paid' ? 'green' : status === 'pending' ? 'amber' : 'red'}
-                      />
-                    </div>
-                  </td>
-                  <td className={classes}>
-                    <div className="flex items-center gap-3">
-                      <div className="h-9 w-12 rounded-md border border-blue-gray-50 p-1">
-                        <Avatar
-                          src={
-                            account === 'visa'
-                              ? 'https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/logos/visa.png'
-                              : 'https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/logos/mastercard.png'
-                          }
-                          size="sm"
-                          alt={account}
-                          variant="square"
-                          className="h-full w-full object-contain p-1"
-                        />
-                      </div>
-                      <div className="flex flex-col">
-                        <Typography
-                          variant="small"
-                          color="blue-gray"
-                          className="font-normal capitalize">
-                          {account.split('-').join(' ')} {accountNumber}
-                        </Typography>
-                        <Typography
-                          variant="small"
-                          color="blue-gray"
-                          className="font-normal opacity-70">
-                          {expiry}
-                        </Typography>
-                      </div>
-                    </div>
-                  </td>
-                  <td className={classes}>
-                    <Tooltip content="Edit User">
+                    <Button
+                      onClick={ handleOpen}
+                      onMouseDown={()=> setClickedID(_id)}
+                      variant="outlined"
+                      size="sm"
+                      className="flex items-center gap-3">
+                      Details
+                    </Button>
+
+                    {/* <Tooltip content="Edit User">
                       <IconButton
                         variant="text"
                         color="blue-gray">
                         <PencilIcon className="h-4 w-4" />
                       </IconButton>
-                    </Tooltip>
+                    </Tooltip> */}
                   </td>
                 </tr>
               );
             })}
           </tbody>
         </table>
+        <DetailsModal
+          handleOpen={handleOpen}
+          open={open}
+          clickedID={clickedID}></DetailsModal>
       </CardBody>
       <CardFooter className="flex items-center justify-between border-t border-blue-gray-50 p-4">
         <Button
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -29,7 +29,8 @@ const router = createBrowserRouter([
       },
       {
         path: '/all-toys',
-        element: <AllToys></AllToys>
+        element: <AllToys></AllToys>,
+        loader: () => fetch('http://localhost:5000/toys'),
       },
       {
         path: '/user/add-toy',
